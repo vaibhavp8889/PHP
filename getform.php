6f81114386bc0cb401efbf0417b909a1d676f63b
vaibhavp8889 <133848295+vaibhavp8889@users.noreply.github.com>
2023-05-24 18:52:46 +0530

    Add files via upload (#1)

---
+++ getform.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        input{
+            border-radius: 2px;
+            margin: 3px;
+        }
+        #submit{
+            width: 150px;
+            background-color: greenyellow;
+            
+        }
+    </style>
+</head>
+<body>
+    <form action="get.php" method="get">
+        <input type="text" value="" name="name" id="name">
+        <label for="name">Name</label><br>
+        <input type="text" name="age" id="age">
+        <label for="age">Age</label><br>
+        <input type="submit" name="save" value="Submit" id="submit">
+    </form>
+</body>
+</html>
